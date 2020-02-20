e588ed29413a5b4732fd0e9ebd1c2a6a080a0a40
Taki <laszlo@letmewebyou.com>
2020-02-20 18:03:47 +0100

    Further

--- FirstLesson/react-first-lesson/src/App.js
+++ FirstLesson/react-first-lesson/src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from "react";
 import "./App.css";
+import Person from "./Person/Person";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1> Hi I am a React app! </h1>{" "}
+        <h1> Hi I am a React app! </h1>
+        <h1>Another Heading</h1>
+        <Person />
       </div>
     );
 
