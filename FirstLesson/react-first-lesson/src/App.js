2fc6ec9e00162ccc9309cd91deca6c58bce06f30
Laszlo Takacs <laszlo@letmewebyou.com>
2020-02-26 11:47:53 +0100

    Functional compontents added

--- FirstLesson/react-first-lesson/src/App.js
+++ FirstLesson/react-first-lesson/src/App.js
@@ -1,23 +1,45 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import "./App.css";
 import Person from "./Person/Person";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <h1> Hi I am a React app! </h1>
-        <h1>Another Heading</h1>
-        <Person />
+function App() {
+  const [name, setName] = useState("Taki");
+  const [showPersons, setShowPersons] = useState(false);
+  const [emberek, setEmberek] = useState([
+    { name: "Taki", age: 12 },
+    { name: "Taki2", age: 14 },
+    { name: "Taki3", age: 13 }
+  ]);
+
+  function setNameHandler(event) {
+    setName(event.target.value);
+  }
+
+  function togglePersonsHandler(props) {
+    setShowPersons(prevValue => !prevValue);
+    console.log(showPersons);
+  }
+
+  let persons = null;
+
+  if (showPersons) {
+    persons = ( //Persons to be shown
+      <div>
+        {emberek.map(ember => {
+          return <Person name={ember.name} age={ember.age} />;
+        })}
       </div>
     );
-
-    // return React.createElement(
-    //   "div",
-    //   null,
-    //   React.createElement("h1", { className: "App" }, "Hi, I am a React App!!") //Create element without JSX method.
-    // );
   }
+
+  return (
+    <div className="App">
+      <h1> Hi I am a React app! </h1>
+      <h1>Another Heading</h1>
+      <button onClick={togglePersonsHandler}>Show Persons</button>
+      {persons}
+    </div>
+  );
 }
 
 export default App;
