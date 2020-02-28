629bee84241720a2f0e4b259e9af02026f75d0c6
Taki <laszlo@letmewebyou.com>
2020-02-28 15:52:55 +0100

    Added scss to the package, finished dynamic list

--- FirstLesson/react-first-lesson/src/App2.jsx
+++ FirstLesson/react-first-lesson/src/App2.jsx
@@ -5,34 +5,41 @@ import Person from "./Person/Person";
 class App2 extends Component {
   state = {
     persons: [
-      { name: "Taki", age: 12 },
-      { name: "Taki2", age: 14 },
-      { name: "Taki3", age: 13 },
-      { name: "Taki4", age: 16 }
+      { id: "a", name: "Taki", age: 12 },
+      { id: "b", name: "Taki2", age: 14 },
+      { id: "c", name: "Taki3", age: 13 },
+      { id: "d", name: "Taki4", age: 16 }
     ],
     otherState: "Some other value",
     showPersons: false
   };
 
-  // switchNameHandler = newName => {
-  //   this.setState({
-  //     persons: [
-  //       { name: newName, age: 12 },
-  //       { name: "Taki2", age: 14 },
-  //       { name: "Taki3", age: 13 }
-  //     ]
-  //   });
-  // };
+  deletePersonHandler = personIndex => {
+    // const persons = this.state.persons.slice(); //one method
+    const persons = [...this.state.persons];
+    persons.splice(personIndex, 1);
+    this.setState({ persons: persons });
+  };
 
-  // nameChangedHandler = event => {
-  //   this.setState({
-  //     persons: [
-  //       { name: "Max", age: 12 },
-  //       { name: event.target.value, age: 14 },
-  //       { name: "Taki3", age: 13 }
-  //     ]
-  //   });
-  // };
+  nameChangedHandler = (event, id) => {
+    const personIndex = this.state.persons.findIndex(ember => {
+      return ember.id === id;
+    }); //find the index of the person
+
+    const person = {
+      ...this.state.persons[personIndex]
+    }; //person const will get the persons[0] or other number depending on the index
+
+    person.name = event.target.value; //will get the value from the input
+
+    const persons = [...this.state.persons]; //create persons array (to not modify the original state)
+    persons[personIndex] = person; // updates the person const
+
+    this.setState({
+      //set the state of persons to the newly created persons
+      persons: persons
+    });
+  };
 
   togglePersonsHandler = () => {
     //changes the state of showPersons from true to false , forth and back
@@ -45,13 +52,21 @@ class App2 extends Component {
       border: "1px solid black"
     };
 
-    let persons = null; // set the primary calue for persons, if null it shows nothing
+    let persons = null; // set the primary value for persons, if null it shows nothing
 
     if (this.state.showPersons) {
       persons = (
         <div>
-          {this.state.persons.map(person => {
-            return <Person name={person.name} age={person.age} />;
+          {this.state.persons.map((person, index) => {
+            return (
+              <Person
+                key={person.id}
+                click={() => this.deletePersonHandler(index)}
+                name={person.name}
+                age={person.age}
+                changed={event => this.nameChangedHandler(event, person.id)}
+              />
+            );
           })}
         </div>
       );
--- FirstLesson/react-first-lesson/src/Person/Person.js
+++ FirstLesson/react-first-lesson/src/Person/Person.js
@@ -2,11 +2,14 @@ import React from "react";
 
 const person = props => {
   return (
-    <div>
-      <p>Username: {props.name}</p>
-      <p>Age: {props.age}</p>
+    <div className="Person">
+      <p onClick={props.click}>
+        Username: {props.name} Age: {props.age}
+      </p>
+
       <p>{props.child}</p>
-      <input type="text" onChange={props.change} />
+      <input placholder="Name" value={props.name} type="text" onChange={props.changed} />
+      {/* <input placholder="Age" value={props.age} type="text" /> */}
     </div>
   );
 };
